aaa2b83deb4eeac530bbd38e6f96b946d98ae7f6
Jordan Pisaniello <jpisaniello@pm.me>
2023-05-30 13:25:30 -0400

    remove steam whats new

---
+++ remove-steam-whatsnew.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# https://github.com/zero318/BegoneWhatsNew/issues/9#issuecomment-1528905628
+sed -i 's/.libraryhome_UpdatesContainer_17uEB{box-sizing:border-box;padding:16px 24px 0px 24px;position:relative;height:324px;/.libraryhome_UpdatesContainer_17uEB{box-sizing:border-box;padding:16px 24px 0px 24px;position:relative;display:none;/' ~/.local/share/Steam/steamui/css/sp.css
